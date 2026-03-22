93bc3777f8b936045f55ba313738334732c0acc9
D400L <dhawalpawanarkar123@gmail.com>
2026-03-22 15:30:51 +0530

    Interface decalring login , register , getUser

--- backend/java/src/main/java/com/scribehealth/service/AuthService.java
+++ backend/java/src/main/java/com/scribehealth/service/AuthService.java
@@ -1,10 +1,8 @@
 package com.scribehealth.service;
-
 import com.scribehealth.dto.AuthResponse;
 import com.scribehealth.dto.LoginRequest;
 import com.scribehealth.dto.RegisterRequest;
 import com.scribehealth.model.User;
-
 public interface AuthService {
 
     AuthResponse login(LoginRequest request);
