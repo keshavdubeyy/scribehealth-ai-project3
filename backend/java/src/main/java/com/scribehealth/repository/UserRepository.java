45c99a46778edd5ae777f3fde41c35f3f644af37
D400L <dhawalpawanarkar123@gmail.com>
2026-03-22 15:27:45 +0530

    DB interface , findByEmail , exist query methods

--- backend/java/src/main/java/com/scribehealth/repository/UserRepository.java
+++ backend/java/src/main/java/com/scribehealth/repository/UserRepository.java
@@ -1,10 +1,7 @@
 package com.scribehealth.repository;
-
 import com.scribehealth.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
-
 import java.util.Optional;
-
 public interface UserRepository extends MongoRepository<User, String> {
 
     Optional<User> findByEmail(String email);
